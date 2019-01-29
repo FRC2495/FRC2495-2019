f635af5c6baafdaeeded7134d4462870e2a510e5
gartaud <GabrielArtaud@comcast.net>
2019-01-29 14:10:06 -0500

    Cleaned up Robot class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,9 @@
 
 package frc.robot;
 
-//import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -35,17 +35,45 @@ import frc.robot.subsystems.*;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-
   // IMPORTANT MAKE SURE THAT THIS CONSTANT IS SET TO TRUE IF USING COMPETITION BOT!
 	// use this constant to switch between competition and practice bot
   public static final boolean COMPETITION_BOT_CONFIG = true;
 
   public static OI oi;
   
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+	Command m_autonomousCommand;
+	
+	// choosers (for auton)
+	
+	public static final String AUTON_DO_NOTHING = "Do Nothing";
+	public static final String AUTON_CUSTOM = "My Auto";
+	private String autonSelected;
+	private SendableChooser<String> autonChooser = new SendableChooser<>();
+	
+	public static final String START_POSITION_LEFT = "Left";
+	public static final String START_POSITION_CENTER = "Center";
+	public static final String START_POSITION_RIGHT = "Right";
+	private String startPosition;
+	private SendableChooser<String> startPositionChooser = new SendableChooser<>();
+	
+	public static final String CAMERA_OPTION_USE_ALWAYS = "Always";
+	public static final String CAMERA_OPTION_USE_OPEN_LOOP_ONLY = "Open Loop Only";
+	public static final String CAMERA_OPTION_USE_CLOSED_LOOP_ONLY = "Closed Loop Only";
+	public static final String CAMERA_OPTION_USE_NEVER = "Never";
+	private String cameraOption;
+	private SendableChooser<String> cameraOptionChooser = new SendableChooser<>();
+	
+	public static final String SONAR_OPTION_USE_ALWAYS = "Always";
+	public static final String SONAR_OPTION_USE_RELEASE_ONLY = "Release Only";
+	public static final String SONAR_OPTION_USE_GRASP_ONLY = "Grasp Only";
+	public static final String SONAR_OPTION_USE_NEVER = "Never";
+	private String sonarOption;
+	private SendableChooser<String> sonarOptionChooser = new SendableChooser<>();
+	
+	public static final String GRASPER_OPTION_RELEASE = "Release";
+	public static final String GRASPER_OPTION_DONT_RELEASE = "Don't Release"; 
+	private String releaseSelected;
+	private SendableChooser<String> releaseChooser = new SendableChooser<>();
 
 
 	// sensors
@@ -84,11 +112,35 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+		// choosers (for auton)
+		
+		autonChooser.setDefaultOption("Do Nothing", AUTON_DO_NOTHING);
+		autonChooser.addOption("My Auto", AUTON_CUSTOM);
+		SmartDashboard.putData("Auto choices", autonChooser);
+		
+		startPositionChooser.setDefaultOption("Left", START_POSITION_LEFT);
+		startPositionChooser.addOption("Center", START_POSITION_CENTER);
+		startPositionChooser.addOption("Right", START_POSITION_RIGHT);
+		SmartDashboard.putData("Start positions", startPositionChooser);
+		
+		cameraOptionChooser.addOption("Always", CAMERA_OPTION_USE_ALWAYS);
+		cameraOptionChooser.setDefaultOption("Open Loop Only", CAMERA_OPTION_USE_OPEN_LOOP_ONLY);
+		cameraOptionChooser.addOption("Closed Loop Only", CAMERA_OPTION_USE_CLOSED_LOOP_ONLY);
+		cameraOptionChooser.addOption("Never", CAMERA_OPTION_USE_NEVER);		
+		SmartDashboard.putData("Camera options", cameraOptionChooser);
+		
+		sonarOptionChooser.setDefaultOption("Always", SONAR_OPTION_USE_ALWAYS);
+		sonarOptionChooser.addOption("Release Only", SONAR_OPTION_USE_RELEASE_ONLY);
+		sonarOptionChooser.addOption("Grasp Only", SONAR_OPTION_USE_GRASP_ONLY);		
+		sonarOptionChooser.addOption("Never", SONAR_OPTION_USE_NEVER);
+		SmartDashboard.putData("Sonar options", sonarOptionChooser);
+		
+		releaseChooser.setDefaultOption("Release", GRASPER_OPTION_RELEASE);
+		releaseChooser.addOption("Don't release", GRASPER_OPTION_DONT_RELEASE);
+		SmartDashboard.putData("Release options", releaseChooser);
 
-    // sensors
+
+		// sensors
 			
 		sonar = new Sonar(Ports.Analog.SONAR); 
 			
@@ -142,25 +194,38 @@ public class Robot extends TimedRobot {
    * LabVIEW Dashboard, remove all of the chooser code and uncomment the
    * getString line to get the auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+	 * <p>You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+		autonSelected = autonChooser.getSelected();
+		System.out.println("Auton selected: " + autonSelected);
+		
+		startPosition = startPositionChooser.getSelected();
+		System.out.println("Start position: " + startPosition);
+		
+		cameraOption = cameraOptionChooser.getSelected();
+		System.out.println("Camera option: " + cameraOption);
+		
+		sonarOption = sonarOptionChooser.getSelected();
+		System.out.println("Sonar option: " + sonarOption);
+		
+		releaseSelected = releaseChooser.getSelected();
+		System.out.println("Release chosen: " + releaseSelected);
+		
+		//At this point we should know what auto run, where we started, and where our plates are located.
+		//So we are ready for autonomousPeriodic to be called.
+		updateToSmartDash();
 
-		switch (m_autoSelected) {
-      case kCustomAuto:
-        // start custom auto command here
-        break;
-      case kDefaultAuto:
-      default:
-        // start default auto command here
-        break;
-    }
+		m_autonomousCommand = null; // TODO decide which command to run based on user's selection
+
+
+		// schedule the autonomous command (example)
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.start();
+		}
   }
 
 	/**
@@ -173,6 +238,13 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
 	}
 
   /**
@@ -180,7 +252,12 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+		camera.acquireTargets(false);
+		
+    Scheduler.getInstance().run();
+    
+		camera.acquireTargets(false);
+		updateToSmartDash();
   }
 
   /**
@@ -196,10 +273,60 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void disabledPeriodic() {	
-		Scheduler.getInstance().run();
+		camera.acquireTargets(false);
+
+    Scheduler.getInstance().run();
+    
+    camera.acquireTargets(false);
+		updateToSmartDash();
   }
   
   public void updateToSmartDash()
 	{
+		// Send Gyro val to Dashboard
+		SmartDashboard.putNumber("Gyro Value", gyro.getAngle());
+			
+		SmartDashboard.putNumber("Right Value", drivetrain.getRightPosition());
+		SmartDashboard.putNumber("Left Value", drivetrain.getLeftPosition());
+		SmartDashboard.putNumber("Right Enc Value", drivetrain.getRightEncoderPosition());
+		SmartDashboard.putNumber("Left Enc Value", drivetrain.getLeftEncoderPosition());
+		SmartDashboard.putNumber("Right Enc Velocity", drivetrain.getRightEncoderVelocity());
+		SmartDashboard.putNumber("Left Enc Velocity", drivetrain.getLeftEncoderVelocity());
+		SmartDashboard.putBoolean("isMoving?", drivetrain.isMoving());
+		SmartDashboard.putBoolean("isTurning?", drivetrain.isTurning());
+		SmartDashboard.putBoolean("isStalled?", drivetrain.isStalled());
+		
+		SmartDashboard.putBoolean("isCompromised?", DriverStation.getInstance().isDisabled());
+		
+		SmartDashboard.putNumber("Distance to Target", camera.getDistanceToTargetUsingVerticalFov());
+		SmartDashboard.putNumber("Angle to Target", camera.getAngleToTurnToTarget());
+		SmartDashboard.putNumber("Distance to Target Using Horizontal FOV", camera.getDistanceToTargetUsingHorizontalFov());
+		
+		SmartDashboard.putBoolean("Elevator Limit Switch", elevatorControl.getLimitSwitchState());
+		SmartDashboard.putNumber("Elevator Position", elevatorControl.getPosition());
+		SmartDashboard.putNumber("Elevator Enc Position", elevatorControl.getEncoderPosition());
+		SmartDashboard.putBoolean("Elevator IsHoming?", elevatorControl.isHoming());
+		SmartDashboard.putBoolean("Elevator IsMoving?", elevatorControl.isMoving());
+		SmartDashboard.putNumber("Elevator Target", elevatorControl.getTarget());
+		SmartDashboard.putBoolean("Elevator Has Been Homed?", elevatorControl.hasBeenHomed());
+		SmartDashboard.putBoolean("Elevator isDown", elevatorControl.isDown());
+		SmartDashboard.putBoolean("Elevator isMidway", elevatorControl.isMidway());
+		SmartDashboard.putBoolean("Elevator isUp", elevatorControl.isUp());
+		
+		SmartDashboard.putBoolean("Gyro Manually Calibrated?",hasGyroBeenManuallyCalibratedAtLeastOnce);
+		
+		SmartDashboard.putNumber("Tilt", accelerometer.getTilt());
+		
+		SmartDashboard.putNumber("Range to target", sonar.getRangeInInches());
+		SmartDashboard.putNumber("Sonar Voltage", sonar.getVoltage()); 
+		
+		SmartDashboard.putBoolean("Grasper IsGrasping?", grasper.isGrasping());
+		SmartDashboard.putBoolean("Grasper IsReleasing?", grasper.isReleasing());
+		
+		SmartDashboard.putString("Auton selected", autonChooser.getSelected());	
+		SmartDashboard.putString("Start position", startPositionChooser.getSelected());
+		SmartDashboard.putString("Camera option", cameraOptionChooser.getSelected());
+		SmartDashboard.putString("Sonar option", sonarOptionChooser.getSelected());
+		SmartDashboard.putString("Release chosen", releaseChooser.getSelected());
   }
 }
