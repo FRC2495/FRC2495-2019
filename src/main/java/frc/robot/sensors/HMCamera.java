856c02ec68a61d5d2541a7238a31ffb6d0b01d1c
Mr. A <GabrielArtaud@comcast.net>
2019-02-03 16:04:50 -0500

    added comments pointing out the need for thread safety

--- src/main/java/frc/robot/sensors/HMCamera.java
+++ src/main/java/frc/robot/sensors/HMCamera.java
@@ -263,6 +263,8 @@ public class HMCamera implements PIDSource, ICamera {
 		return PIDSourceType.kDisplacement;
 	}
 	
+	// The PIDController that will call this method runs in a different thread than the scheduler thread,
+	// so it is important that this class be made thread-safe. This is why all the public methods are synchronized.
 	public synchronized double pidGet()
 	{
 		acquireTargets(false); // we don't want to wait but the lag might be problematic
@@ -280,6 +282,8 @@ public class HMCamera implements PIDSource, ICamera {
 		return PIDSourceType.kDisplacement;
 	}
 	
+	// The PIDController that will call this method runs in a different thread than the scheduler thread,
+	// so it is important that this class be made thread-safe. This is why all the public methods are synchronized.
 	public synchronized double pidGet2()
 	{
 		acquireTargets(false); // we don't want to wait but the lag might be problematic
