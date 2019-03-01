0cfdf6a5354f15071d5888ee753f61dc5d1c0a37
Frc2495 <FRC2495@gmail.com>
2019-03-01 16:14:37 -0500

    Implamented Hab 1 Right to Rocket auton path

---
+++ src/main/java/frc/robot/auton/CargoHab1RightToRocket.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.auton;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.*;
+
+
+public class CargoHab1RightToRocket extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public CargoHab1RightToRocket() {
+    // Move foward 133 inches
+    addSequential(new DrivetrainMoveDistance(133));
+
+    //Vision Delivery (to CSRF)
+    addSequential(new CargoDeliverShip());
+
+    //Move back 24 inches
+    //addSequential(new DrivetrainMoveDistance(-24));
+
+    //Turn 90 degress to the right
+    addSequential(new DrivetrainTurnAngleUsingPidController(90));
+
+    //Move foward 105 inches foward
+    addSequential(new DrivetrainMoveDistance(105));
+
+    //Turn 90 degrees to the right 
+    addSequential(new DrivetrainTurnAngleUsingPidController(90));
+
+    //Move foward 151 inches
+    addSequential(new DrivetrainMoveDistance(151));
+
+    //Recieve hatchpanel (from RH)
+    addSequential(new HatchPanelPickup());
+
+    //Turn 180 degrees to the right 
+    addSequential(new DrivetrainTurnAngleUsingPidController(180));
+
+    //Move foward 151 inches
+    addSequential(new DrivetrainMoveDistance(151));
+
+    //Turn 90 degress to the left
+    addSequential(new DrivetrainTurnAngleUsingPidController(-90));
+
+    //Move foward 105 inches foward
+    addSequential(new DrivetrainMoveDistance(105));
+
+    //Turn 90 degrees to the right 
+    addSequential(new DrivetrainTurnAngleUsingPidController(90));
+
+    //Move foward 44 inches 
+    addSequential(new DrivetrainMoveDistance(44));
+
+    //Vision Delivery (to CSRF)
+    addSequential(new HatchPanelDeliver());
+  }
+}
